c0c7d2d5d75eae1a1a6ff9500b45cf17a32363da
Kirill Makeev <makeevbang@gmail.com>
2025-03-11 13:20:01 +0300

    P_A step_1

---
+++ page_analyzer/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.route('/')
+def index():
+    return 'Hello'
